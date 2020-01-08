d0684316365712bc6e45790aaaacd1fb3081f799
Yaniv Efraim <yaniv.efraim@gmail.com>
2020-01-08 15:57:49 +0200

    more tests

---
+++ test/typescript/features/transpile-configured-external-javascript/src/component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component = require('untranspiled-dependency/javascript').default;
+
+export default () => (
+  <Component id="transpile-configured-external-javascript" />
+);
