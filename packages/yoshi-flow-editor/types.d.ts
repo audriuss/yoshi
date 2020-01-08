41de3741e6fdcf30cbafe6eaaae562ccf9b01ce2
Dean Shub <deanshub@gmail.com>
2020-01-08 10:15:37 +0200

    adding styleable to the yoshi-flow-editor types

--- packages/yoshi-flow-editor/types.d.ts
+++ packages/yoshi-flow-editor/types.d.ts
@@ -81,6 +81,43 @@ declare module '*.svg' {
   export default src;
 }
 
+declare module '*.st.css' {
+  type StateValue = boolean | number | string;
+
+  interface StateMap {
+    [stateName: string]: StateValue;
+  }
+
+  interface AttributeMap {
+    className?: string;
+    [attributeName: string]: StateValue | undefined;
+  }
+
+  interface InheritedAttributes {
+    className?: string;
+    [props: string]: any;
+  }
+
+  type RuntimeStylesheet = {
+    (
+      className: string,
+      states?: StateMap,
+      inheritedAttributes?: InheritedAttributes,
+    ): AttributeMap;
+    $root: string;
+    $namespace: string;
+    $depth: number;
+    $id: string | number;
+    $css?: string;
+
+    $get(localName: string): string | undefined;
+    $cssStates(stateMapping?: StateMap | null): StateMap;
+  } & { [localName: string]: string };
+
+  const stylesheet: RuntimeStylesheet;
+  export default stylesheet;
+}
+
 declare module '*.css' {
   const classes: { [key: string]: string };
   export = classes;
@@ -111,41 +148,4 @@ declare module '*.graphql' {
 declare module '*.inline.worker.ts' {
   const value: any;
   export = value;
-}
-
-type StateValue = boolean | number | string;
-
-interface StateMap {
-  [stateName: string]: StateValue;
-}
-
-interface AttributeMap {
-  className?: string;
-  [attributeName: string]: StateValue | undefined;
-}
-
-interface InheritedAttributes {
-  className?: string;
-  [props: string]: any;
-}
-
-type RuntimeStylesheet = {
-  (
-    className: string,
-    states?: StateMap,
-    inheritedAttributes?: InheritedAttributes,
-  ): AttributeMap;
-  $root: string;
-  $namespace: string;
-  $depth: number;
-  $id: string | number;
-  $css?: string;
-
-  $get(localName: string): string | undefined;
-  $cssStates(stateMapping?: StateMap | null): StateMap;
-} & { [localName: string]: string };
-
-declare module '*.st.css' {
-  const stylesheet: RuntimeStylesheet;
-  export default stylesheet;
 }
